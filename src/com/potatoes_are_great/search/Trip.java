595e2815d515e168c974a5101361aea575ca6f71
timmist24 <xtimmist24#@gmail.com>
2021-04-29 01:13:22 +0100

    evening commit2, little bet extra on Trip

--- src/com/potatoes_are_great/search/Trip.java
+++ src/com/potatoes_are_great/search/Trip.java
@@ -6,12 +6,13 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 public class Trip {
-    private static final Pattern PATTERN = Pattern.compile("(\\d+),(\\d+),(.+),(.+),(-?\\d\\d\\d?.\\d*),(-?\\d\\d\\d?.\\d*),([A-Z][A-Z]\\s\\d\\d),(.*),(\\d),(\\d*)");
+    private static final Pattern PATTERN = Pattern.compile("(\\d+),(\\d+),(\\d\\d?:\\d\\d:\\d\\d),(\\d\\d?:\\d\\d:\\d\\d),(\\d+),(.*),(\\d),(\\d),(\\d+.\\d*)?"); //TODO repair last parretn
 
     private final List<Stop> stops = new ArrayList<>();
     private final List<Time[]> time = new ArrayList<>();
-    //private final
-    private final int id, pPype, dType;
+    private final List<Double> dist = new ArrayList<>();
+    private final List<Integer[]> pDTypes = new ArrayList<>();
+    private final int id;
 
     //TODO set up trip stuff
 }
